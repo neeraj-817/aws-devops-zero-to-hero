1aef7a1869c81f6f7eeb16658c134606dae98d1a
neeraj-817 <104046556+neeraj-817@users.noreply.github.com>
2023-10-16 09:14:17 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, neeraj! after codepeline'
+    return 'Hello, neeraj rawat! after codepeline'
 
 if __name__ == '__main__':
     app.run()
