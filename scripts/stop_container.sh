563530c53177aa2b17f9665482c3bf1fac178f76
neeraj-817 <104046556+neeraj-817@users.noreply.github.com>
2023-12-03 10:18:18 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,9 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+#!/bin/bash
+set -e
+
+# Stop the running container (if any)
+containerid=docker ps | awk -F " " '{print $1}'`
+docker rm -f $containerid
